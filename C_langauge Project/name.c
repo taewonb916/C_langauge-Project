c9a94ad05dfcf7338ec727411b601bb330236c23
Taewon Park <taewonb916@gmail.com>
2021-08-22 16:31:05 +0900

    Revert "scanf"
    This reverts commit 1baeaaef8d1419fbd89a14d9f7f2432a02049fd9.

--- C_langauge Project/name.c
+++ C_langauge Project/name.c
@@ -3,9 +3,5 @@
 int main(void)
 {
 	printf("È«±æµ¿ \nÈ« ±æ µ¿ \nÈ«  ±æ  µ¿ \n");
-	printf("name : ¹Ú ÅÂ¿ø\n");
-	printf("adress : Kagoshima\n");
-	printf("number : 080-1111-2222 \n");
-
 	return 0;
 }
